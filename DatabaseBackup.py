278a3b10c78588867e7206a43c6c5dc3e475f1b6
TechWZ <TechArtisan@126.com>
2021-01-14 14:28:45 +0800

    update python scripts

--- DatabaseBackup.py
+++ DatabaseBackup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import os
+import sys
 import time
+import os
 
 time_str=time.strftime("%Y%m%d%H%M%S",time.localtime())
 
-bashCommand = 'mysqldump -h2402:f000:1:9e1::9:248 -unidtga -pNIDTGA_802.1x campus6 > /Database_Backup/campus6_'+time_str+'_backup.sql'
+bashCommand = 'mysqldump -h2402:f000:1:9e1::9:248 -unidtga -pNIDTGA_802.1x campus6 > /DatabaseBackup/BackupSQL/campus6_'+time_str+'_backup.sql'
 os.system(bashCommand)
--- DeleteExpireFile.py
+++ DeleteExpireFile.py
@@ -4,10 +4,10 @@ import os
 import time
 
 currentTime = time.time()
-operation_dir='/Database_Backup/BackupSQL'
+operation_dir='/DatabaseBackup/BackupSQL'
 for path,subDirNames,fileNames in os.walk(operation_dir):
     for fileName in fileNames:
         filePath = os.path.join(path,fileName)
         fileCtime = os.stat(filePath).st_ctime
-        if currentTime-fileCtime > 2592000:
+        if currentTime-fileCtime > 2419200:
             os.system("rm -f "+filePath)
